9b3a73a74cc8ed2ae58bc6f7db3070f53fd1e63e
Murilo <murilo.ribeiro@alunos.unicesumar.edu.br>
2021-11-11 16:57:45 -0300

    corrections2

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@ let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
 menuToggle.addEventListener("click", () => {
+
+
     menuSection.classList.toggle("on", show)
     show = !show
 })
